68a17f49a408c8d5c4dda0bdf24103dfab280b55
Sean-Mecham <you@example.comsmecham@nic.edu>
2023-10-25 10:41:16 -0700

    whatever

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main()
     cout<<"Hello\n";
     void displayArray();
     cout<<"Fare thee well.";
+    cout<<"Conflict test for Sean";
 
     cout<<"Functions are working great\n"; 
     return 0;
